3b957ba8b0139f15de0ca6dae4ee20c1b5777d76
pgauna314 <pablogauna@gmail.com>
2025-12-22 12:39:02 -0300

    prueba

--- proyectomayer/app/app.py
+++ proyectomayer/app/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Configuración de la página
-st.set_page_config(page_title="Proyecto TERMO", layout="wide")
+st.set_page_config(page_title="Proyecto TERMODDINAMICA", layout="wide")
 
 # Título Principal
 st.title("🏗️ Proyecto TERMO")

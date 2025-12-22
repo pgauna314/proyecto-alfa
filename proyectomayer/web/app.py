f394201adf4221f984f2b424cc032361d9507d0f
pgauna314 <139083452+pgauna314@users.noreply.github.com>
2025-12-22 07:02:50 -0300

    app py actualizado

--- proyectomayer/web/app.py
+++ proyectomayer/web/app.py
@@ -4,16 +4,19 @@ from modules.inicio import mostrar_inicio
 from modules.matriz import mostrar_matriz
 from modules.capitulo2 import mostrar_cap2
 from modules.autor import mostrar_autor
+# NUEVO: Importamos el laboratorio
+from modules.laboratorio import mostrar_laboratorio
 
 st.set_page_config(page_title="Proyecto MAYER", layout="wide", page_icon="⚛️")
 
 # Barra Lateral
 with st.sidebar:
     st.title("🏗️ Proyecto MAYER")
-    # AGREGUÉ "Sobre el Autor" AQUÍ ABAJO:
+    # AGREGAMOS "Laboratorio de Propiedades" al menú:
     menu = st.radio("Navegación:", [
         "Inicio", 
         "Matriz Energética", 
+        "Laboratorio de Propiedades", # <-- NUEVA OPCIÓN
         "Capítulo II: Sistemas",
         "Sobre el Autor"
     ])
@@ -26,10 +29,10 @@ if menu == "Inicio":
     mostrar_inicio()
 elif menu == "Matriz Energética":
     mostrar_matriz()
+elif menu == "Laboratorio de Propiedades": # <-- NUEVA RUTA
+    mostrar_laboratorio()
 elif menu == "Capítulo II: Sistemas":
     mostrar_cap2()
 elif menu == "Sobre el Autor":         
     mostrar_autor()
-
-
 
